d9841d59cf0d3892700f130636fe3a2ad008f39c
patilsakshi438-creator <patilsakshi438@gmail.com>
2026-02-16 14:37:13 +0530

    Added image component

---
+++ src/component/Images.js
@@ -0,0 +1,12 @@
+
+const Image =(props)=>{
+    return(
+        <li>
+            <ul>Sakshi Patil</ul>
+            <ul>Komal Patil</ul>
+            <ul>Rutuja Patil</ul>
+        </li>
+    )
+}
+
+export default Image;
