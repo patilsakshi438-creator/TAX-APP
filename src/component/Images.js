1130d22c125a05cb507a2e73db12b90246bd4eb0
patilsakshi438-creator <patilsakshi438@gmail.com>
2026-02-16 14:44:23 +0530

    added new changes as per sit instruction

--- src/component/Images.js
+++ src/component/Images.js
@@ -3,8 +3,7 @@ const Image =(props)=>{
     return(
         <li>
             <ul>Sakshi Patil</ul>
-            <ul>Komal Patil</ul>
-            <ul>Rutuja Patil</ul>
+           
         </li>
     )
 }
